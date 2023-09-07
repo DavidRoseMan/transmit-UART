9d2d0c9ff9e172fe0249c720c0d7de80545f07fb
DavidRoseMan <107785201+DavidRoseMan@users.noreply.github.com>
2023-09-07 17:50:27 +0800

    fix some bugs and add the function "vmyprintf"

--- transmit.c
+++ transmit.c
@@ -1,6 +1,5 @@
 #include "transmit.h"
 #include "variable.h"
-#include <STDARG.H>
 
 void init_UART(Uart_type *example)
 {
@@ -100,8 +99,8 @@ void myprintf(Uart_type *uart_example, char *str, ...) reentrant
 {
     char *char_temp;
     va_list ap;
-    unsigned int i;
-    unsigned int number_get;
+    int i;
+    int number_get;
     unsigned char head_flag; // print start from the number's head_value which is not zero.
 
     char_temp = str;
@@ -128,20 +127,100 @@ void myprintf(Uart_type *uart_example, char *str, ...) reentrant
                 char_temp++;
                 continue;
             }
-            number_get = va_arg(ap, unsigned int);
-            i = 10000;
-            head_flag = 1;
-            while (i)
+            number_get = va_arg(ap, int);
+            if (number_get == 0)
             {
-                if (head_flag == 1 && number_get / i != 0)
-                    head_flag = 0;
-                if (!head_flag)
-                    sendbyte_UART(uart_example, (unsigned char)('0' + number_get / i));
-                number_get %= i;
-                i /= 10;
+                sendbyte_UART(uart_example, (unsigned char)'0');
+            }
+            else
+            {
+                if (number_get < 0)
+                {
+                    i = -10000;
+                    sendbyte_UART(uart_example, (unsigned char)'-');
+                }
+                else
+                {
+                    i = 10000;
+                }
+                head_flag = 1;
+                while (i)
+                {
+                    if (head_flag == 1 && number_get / i != 0)
+                        head_flag = 0;
+                    if (!head_flag)
+                        sendbyte_UART(uart_example, (unsigned char)('0' + number_get / i));
+                    number_get %= i;
+                    i /= 10;
+                }
             }
             char_temp++;
         }
     }
     va_end(ap);
+    // sendbyte_UART(uart_example, (unsigned char)'\0');
+}
+
+void vmyprintf(Uart_type *uart_example, char *str, va_list ap) reentrant
+{
+    char *char_temp;
+    int i;
+    int number_get;
+    unsigned char head_flag; // print start from the number's head_value which is not zero.
+
+    char_temp = str;
+    while (*char_temp != '\0')
+    {
+        if (*char_temp != '%')
+        {
+            sendbyte_UART(uart_example, (unsigned char)*char_temp);
+            char_temp++;
+        }
+        else
+        {
+            char_temp++;
+            if (*char_temp == '\0')
+            {
+                sendbyte_UART(uart_example, (unsigned char)'%');
+                break;
+            }
+            else if (*char_temp != 'd')
+            {
+                sendbyte_UART(uart_example, (unsigned char)'%');
+                sendbyte_UART(uart_example, (unsigned char)*char_temp);
+                char_temp++;
+                continue;
+            }
+            number_get = va_arg(ap, int);
+            if (number_get == 0)
+            {
+                sendbyte_UART(uart_example, (unsigned char)'0');
+            }
+            else
+            {
+                if (number_get < 0)
+                {
+                    i = -10000;
+                    sendbyte_UART(uart_example, (unsigned char)'-');
+                }
+                else
+                {
+                    i = 10000;
+                }
+                head_flag = 1;
+                while (i)
+                {
+                    if (head_flag == 1 && number_get / i != 0)
+                        head_flag = 0;
+                    if (!head_flag)
+                        sendbyte_UART(uart_example, (unsigned char)('0' + number_get / i));
+                    number_get %= i;
+                    i /= 10;
+                }
+            }
+            char_temp++;
+        }
+    }
+    va_end(ap);
+    // sendbyte_UART(uart_example, (unsigned char)'\0');
 }
--- transmit.h
+++ transmit.h
@@ -1,7 +1,9 @@
 #ifndef _transmit_h
 #define _transmit_h
 
-#define sizeof_Uart_buffer  255
+#define sizeof_Uart_buffer 255
+
+#include <stdarg.h>
 
 typedef xdata struct Uart_struct
 {
@@ -16,7 +18,7 @@ void sendbyte_UART(Uart_type *example, unsigned char Data) reentrant;
 void UART_Process_Send(Uart_type *example, unsigned char number_uart);
 void UART_Process_Send_ITR(Uart_type *example, unsigned char number_uart) reentrant;
 //----------------------------------------------------------------------------------------
-void myprintf(Uart_type *example, char *str, ...) reentrant;
+void myprintf(Uart_type *uart_example, char *str, ...) reentrant;
+void vmyprintf(Uart_type *uart_example, char *str, va_list ap) reentrant;
 
 #endif
-
